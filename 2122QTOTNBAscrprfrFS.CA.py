742141b0d565026442c6e366f6673e838f5e8426
martinwilliamsiet <dailybread@scorequest.info>
2024-05-17 10:28:25 -0400

    Updated chrom test and chrome driver version added try loop for debugging and optional debug comments

--- 2122QTOTNBAscrprfrFS.CA.py
+++ 2122QTOTNBAscrprfrFS.CA.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException #debugging
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys  # scroll
@@ -19,125 +21,154 @@ from selenium.common.exceptions import TimeoutException
 # Get the directory of the current script
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
-# Define the path to the Chrome driver executable relative to the current script directory
-chrome_driver_path = os.path.join(current_directory, "chromedriver-win64 (2)", "chromedriver.exe")
+# Define the path to your custom Chrome executable, using developer test version
+chrome_binary_path = os.path.join(current_directory,"chrome-win64", "chrome.exe")
+
+# Define the path to the Chrome driver executable relative to the current script directory, driver matches chrome test version
+chrome_driver_path = os.path.join(current_directory,"chromedriver-win64", "chromedriver.exe")
+
+# # Check if the path exists -testing
+# if not os.path.exists(chrome_driver_path):
+#     raise FileNotFoundError(f"ChromeDriver not found at path: {chrome_driver_path}")
+
+# Print paths for debugging
+print("Chrome binary path:", chrome_binary_path)
+print("Chrome driver path:", chrome_driver_path)
 
 URL = "https://www.flashscore.ca/basketball/usa/nba-2021-2022/results/"
 
 # Initialize WebDriver
-service = Service(chrome_driver_path)
-service.start()
-chrome_options = webdriver.ChromeOptions()
+chrome_options = Options()
+chrome_options.binary_location = chrome_binary_path
 chrome_options.add_argument("--window-size=1920,1080")
-driver = webdriver.Chrome(service=service, options=chrome_options)
-driver.get(URL)
+# driver = webdriver.Chrome(service=service, options=chrome_options)
+# driver.get(URL)
 
-# Execute JavaScript to dismiss the cookie pop-up
-driver.execute_script("document.querySelector('.ot-pc-refuse-all-handler').click();")
-
-# Wait for the page to fully load after dismissing the pop-up
-WebDriverWait(driver, 5).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ot-floating-bar-show')))
-
-# Define a function to click the "Show more games" button
-def click_show_more_games(driver):
-    show_more_button = driver.find_element(By.CSS_SELECTOR, 'a.event__more.event__more--static')
-    actions = ActionChains(driver)
-    actions.move_to_element(show_more_button).click().perform()
-
-# Define the function to convert time to 24-hour format
-def convert_to_24_hour(time_str):
-    return datetime.strptime(time_str, "%I:%M %p").strftime("%H%M")
-
-# Click the "Show more games" button until it's no longer clickable
-while True:
-    try:
-        click_show_more_games(driver)
-        time.sleep(6)  # Wait for more games to load
-    except:
-        break  # Break the loop if the button is no longer clickable
-
-# Wait for the parent div to load
-parent_div = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'sportName.basketball')))
-
-# Get the HTML content of the parent div
-html_content = parent_div.get_attribute('innerHTML')
-
-# Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(html_content, "html.parser")
-
-# Find all divs within the parent div
-divs_in_parent = soup.find_all('div', class_='event__match')
-
-# Initialize game_data as an empty list
-game_data = []
-
-# Dictionary to map team names to their abbreviations
-team_abbreviations = {
-    "Atlanta Hawks": "ATL", "Boston Celtics": "BOS", "Brooklyn Nets": "BKN", "Charlotte Hornets": "CHH",
-    "Chicago Bulls": "CHI", "Cleveland Cavaliers": "CLE", "Dallas Mavericks": "DAL", "Denver Nuggets": "DEN",
-    "Detroit Pistons": "DET", "Golden State Warriors": "GSW", "Houston Rockets": "HOU", "Indiana Pacers": "IND",
-    "Los Angeles Clippers": "LAC", "Los Angeles Lakers": "LAL", "Memphis Grizzlies": "MEM", "Miami Heat": "MIA",
-    "Milwaukee Bucks": "MIL", "Minnesota Timberwolves": "MIN", "New Orleans Pelicans": "NOP", "New York Knicks": "NYK",
-    "Oklahoma City Thunder": "OKC", "Orlando Magic": "ORL", "Philadelphia 76ers": "PHI", "Phoenix Suns": "PHX",
-    "Portland Trail Blazers": "POR", "Sacramento Kings": "SAC", "San Antonio Spurs": "SAS", "Toronto Raptors": "TOR",
-    "Utah Jazz": "UTA", "Washington Wizards": "WAS"
-}
-
-# Extract game information, inverted from scrape so inverting here
-for div in divs_in_parent:
-    game_info = {}
-    # Extract home team name PARSE ABBR
-    home_team_div = div.find('div', class_='event__participant--home')
-    if home_team_div:
-        away_team_name = home_team_div.text.strip() #swap home/vis here
-        game_info['visitorteamname'] = away_team_name #swap home/vis here
-        away_team_abbr = team_abbreviations.get(away_team_name, 'UNK') #swap home/vis here
-
-    # REMOVE '@', PARSE ABBR Extract away team name
-    away_team_div = div.find('div', class_='event__participant--away')
-    if away_team_div:
-        home_team_name = away_team_div.text.strip().lstrip('@')  #swap home/vis here # Remove leading '@' symbol
-        game_info['hometeamname'] = home_team_name #swap home/vis here
-        home_team_abbr = team_abbreviations.get(home_team_name, 'UNK') #swap home/vis here
-
-    # PARSE MONTH/TIME Extract event time 
-    event_time_div = div.find('div', class_='event__time')
-    if event_time_div:
-        event_time = event_time_div.text.strip().lstrip('AOT')
-        game_info['eventtime'] = event_time
-
-    for i in range(1, 7):  
-        if i <= 4:  # For quarters 1 to 4
-            score_type = 'q'
-        else:  # For overtime periods
-            score_type = 'ot'
-
-        if i <= 4:
-            home_label = f'visitorq{i}' #swap home/vis here
-            visitor_label = f'homeq{i}' #swap home/vis here
-        else:
-            ot_number = i - 4
-            home_label = f'visitorot{ot_number}' #swap home/vis here
-            visitor_label = f'homeot{ot_number}' #swap home/vis here
-
-        home_score_div = div.find('div', class_=f'event__part event__part--home event__part--{i}')
-        visitor_score_div = div.find('div', class_=f'event__part event__part--away event__part--{i}')
-
-        if home_score_div:
-            game_info[home_label] = home_score_div.text.strip()
-        else:
-            game_info[home_label] = None
-
-        if visitor_score_div:
-            game_info[visitor_label] = visitor_score_div.text.strip()
-        else:
-            game_info[visitor_label] = None
+#debugging
+# Attempt to initialize the WebDriver and catch any exceptions
+# try:
+#     driver = webdriver.Chrome(service=service, options=chrome_options)
+# except WebDriverException as e:
+#     print(f"Failed to start WebDriver: {e}")
+#     service.stop()
+#     raise
 
 
-    game_data.append(game_info)
+# Use WebDriver
+try:
+    service = Service(chrome_driver_path)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.get(URL)
+    # Do other tasks with the driver
 
-# Close the WebDriver
-driver.quit()
+
+    # Execute JavaScript to dismiss the cookie pop-up
+    driver.execute_script("document.querySelector('.ot-pc-refuse-all-handler').click();")
+
+    # Wait for the page to fully load after dismissing the pop-up
+    WebDriverWait(driver, 5).until(EC.invisibility_of_element_located((By.CLASS_NAME, 'ot-floating-bar-show')))
+
+    # Define a function to click the "Show more games" button
+    def click_show_more_games(driver):
+        show_more_button = driver.find_element(By.CSS_SELECTOR, 'a.event__more.event__more--static')
+        actions = ActionChains(driver)
+        actions.move_to_element(show_more_button).click().perform()
+
+    # Define the function to convert time to 24-hour format
+    def convert_to_24_hour(time_str):
+        return datetime.strptime(time_str, "%I:%M %p").strftime("%H%M")
+
+    # Click the "Show more games" button until it's no longer clickable
+    while True:
+        try:
+            click_show_more_games(driver)
+            time.sleep(6)  # Wait for more games to load
+        except:
+            break  # Break the loop if the button is no longer clickable
+
+    # Wait for the parent div to load
+    parent_div = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'sportName.basketball')))
+
+    # Get the HTML content of the parent div
+    html_content = parent_div.get_attribute('innerHTML')
+
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    # Find all divs within the parent div
+    divs_in_parent = soup.find_all('div', class_='event__match')
+
+    # Initialize game_data as an empty list
+    game_data = []
+
+    # Dictionary to map team names to their abbreviations
+    team_abbreviations = {
+        "Atlanta Hawks": "ATL", "Boston Celtics": "BOS", "Brooklyn Nets": "BKN", "Charlotte Hornets": "CHH",
+        "Chicago Bulls": "CHI", "Cleveland Cavaliers": "CLE", "Dallas Mavericks": "DAL", "Denver Nuggets": "DEN",
+        "Detroit Pistons": "DET", "Golden State Warriors": "GSW", "Houston Rockets": "HOU", "Indiana Pacers": "IND",
+        "Los Angeles Clippers": "LAC", "Los Angeles Lakers": "LAL", "Memphis Grizzlies": "MEM", "Miami Heat": "MIA",
+        "Milwaukee Bucks": "MIL", "Minnesota Timberwolves": "MIN", "New Orleans Pelicans": "NOP", "New York Knicks": "NYK",
+        "Oklahoma City Thunder": "OKC", "Orlando Magic": "ORL", "Philadelphia 76ers": "PHI", "Phoenix Suns": "PHX",
+        "Portland Trail Blazers": "POR", "Sacramento Kings": "SAC", "San Antonio Spurs": "SAS", "Toronto Raptors": "TOR",
+        "Utah Jazz": "UTA", "Washington Wizards": "WAS"
+    }
+
+    # Extract game information, inverted from scrape so inverting here
+    for div in divs_in_parent:
+        game_info = {}
+        # Extract home team name PARSE ABBR
+        home_team_div = div.find('div', class_='event__participant--home')
+        if home_team_div:
+            away_team_name = home_team_div.text.strip() #swap home/vis here
+            game_info['visitorteamname'] = away_team_name #swap home/vis here
+            away_team_abbr = team_abbreviations.get(away_team_name, 'UNK') #swap home/vis here
+
+        # REMOVE '@', PARSE ABBR Extract away team name
+        away_team_div = div.find('div', class_='event__participant--away')
+        if away_team_div:
+            home_team_name = away_team_div.text.strip().lstrip('@')  #swap home/vis here # Remove leading '@' symbol
+            game_info['hometeamname'] = home_team_name #swap home/vis here
+            home_team_abbr = team_abbreviations.get(home_team_name, 'UNK') #swap home/vis here
+
+        # PARSE MONTH/TIME Extract event time 
+        event_time_div = div.find('div', class_='event__time')
+        if event_time_div:
+            event_time = event_time_div.text.strip().lstrip('AOT')
+            game_info['eventtime'] = event_time
+
+        for i in range(1, 7):  
+            if i <= 4:  # For quarters 1 to 4
+                score_type = 'q'
+            else:  # For overtime periods
+                score_type = 'ot'
+
+            if i <= 4:
+                home_label = f'visitorq{i}' #swap home/vis here
+                visitor_label = f'homeq{i}' #swap home/vis here
+            else:
+                ot_number = i - 4
+                home_label = f'visitorot{ot_number}' #swap home/vis here
+                visitor_label = f'homeot{ot_number}' #swap home/vis here
+
+            home_score_div = div.find('div', class_=f'event__part event__part--home event__part--{i}')
+            visitor_score_div = div.find('div', class_=f'event__part event__part--away event__part--{i}')
+
+            if home_score_div:
+                game_info[home_label] = home_score_div.text.strip()
+            else:
+                game_info[home_label] = None
+
+            if visitor_score_div:
+                game_info[visitor_label] = visitor_score_div.text.strip()
+            else:
+                game_info[visitor_label] = None
+
+
+        game_data.append(game_info)
+
+finally:
+    driver.quit()
+    service.stop()
 
 # Convert game_data into a pandas DataFrame
 games_df = pd.DataFrame(game_data)
